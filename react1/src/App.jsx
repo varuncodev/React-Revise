9204d5f930fd3f016abb8442b584186d73c9b413
varuncodev <varuncodev@gmail.com>
2026-04-09 17:19:11 +0530

    propss and counter

--- react1/src/App.jsx
+++ react1/src/App.jsx
@@ -1,16 +1,31 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import Card1 from './Component/cart'
+import './index.css'
 
 function App() {
   // let counter =15
   let [counter,setCounter] = useState(15)
 
+  
+    let myobj ={
+        username: "varun",
+        age :24,
+
+    }
+    let arr =[1,2,3]
+
+
   const btn1 =()=>{ 
     // console.log("increament",Math.random())
-       setCounter(counter+1)
+    // if(counter<20){
+      // setCounter(counter+1)
+setCounter(prevCounter => prevCounter+1)   //callback
+setCounter(prevCounter => prevCounter+1)
+setCounter(prevCounter => prevCounter+1)
+setCounter(prevCounter => prevCounter+1)
+
+    // }
+       
   }
 
 
@@ -18,7 +33,9 @@ function App() {
     // console.log("Decreament",-1)
     if(counter>0){
       setCounter(counter-1)
+
     }
+
     
   }
 
@@ -26,10 +43,13 @@ function App() {
     <>
      <h1>Hello varun</h1>
      <h2>Count Value: {counter}</h2>
-     <button onClick={btn1}>Increment</button>
-     <br/>
-     <button onClick={btn2}>Decrement</button>
-    </>
+<div className="btn-group">
+  <button onClick={btn1}>Increment</button>
+  <button onClick={btn2}>Decrement</button>
+</div>
+<Card1 username ="sharma" someo ="Click me"/>
+<Card1  username ="sardar" someo ="Learn more"/>
+</>
   )
 }
 
